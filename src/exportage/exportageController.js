03eaa513c0f46ad4296ee5d11f0a85a1f66c0bcf
Arnaud Thimel <thimel@codelutin.com>
2014-11-03 17:31:23 +0100

    Implement profile summary

--- src/exportage/exportageController.js
+++ src/exportage/exportageController.js
@@ -7,6 +7,35 @@ angular.module('zoumProfilerApp')
     })
     .controller('ExportageController', ['$scope', function ($scope) {
 
-}]);
+        $scope.compList = function() {
+            var result = "";
+            angular.forEach($scope.comps, function(comp) {
+                if (comp.reservedFor) {
+                    if (comp.reservedFor === $scope.profile.race) {
+                        result += comp.id + "|";
+                    }
+                } else {
+                    for (var lvl = comp.levels; lvl >= 1; lvl--) {
+                        var compId = comp.id + lvl;
+                        if ($scope.profile.comps[compId] === true) {
+                            if (comp.levels > 1) {
+                                result += compId + "|"; // cdm4
+                            } else {
+                                result += comp.id + "|"; // he
+                            }
+
+                            break;
+                        }
+                    }
+                }
+            });
+            if (result.length > 1) {
+                result = result.substring(0, result.length - 1);
+            }
+            return result;
+        };
+
+
+    }]);
 
 
