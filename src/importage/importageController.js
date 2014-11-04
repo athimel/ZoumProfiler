60d7cd4dc27b8d7b2465d17a0aa0ef7eae8e7a54
Arnaud Thimel <thimel@codelutin.com>
2014-11-04 17:04:17 +0100

    Support light JSON at import

--- src/importage/importageController.js
+++ src/importage/importageController.js
@@ -9,6 +9,17 @@ angular.module('zoumProfilerApp')
 
         $scope.importProfile = function() {
             var newProfile = angular.fromJson($scope.import.json);
+            angular.forEach($scope.comps, function(comp) {
+                if (comp.levels > 1) {
+                    for (var lvl = comp.levels; lvl >= 1; lvl--) {
+                        var compIdHigherLvl = $scope.getCompId(comp, lvl + 1);
+                        if (newProfile.comps[compIdHigherLvl] === true) {
+                            var compId = $scope.getCompId(comp, lvl);
+                            newProfile.comps[compId] = true;
+                        }
+                    }
+                }
+            });
             $scope.profiles.push(newProfile);
             $scope.saveToStorage();
             $scope.reset();
