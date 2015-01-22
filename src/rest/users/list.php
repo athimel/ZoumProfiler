616e89202ffbb80892c445d5758cd3553f672638
Arnaud Thimel <thimel@codelutin.com>
2015-01-22 10:34:38 +0100

    List users

---
+++ src/rest/users/list.php
@@ -0,0 +1,16 @@
+{"users":<?php
+
+    $m = new MongoClient();
+    $db = $m->zoumprofiler;
+
+    $usersColl = $db->users;
+
+    $users = $usersColl->find();
+    $usersArray = array();
+    foreach ($users as $user) {
+        unset($user['password']);
+        array_push($usersArray, $user);
+    }
+    echo json_encode($usersArray);
+?>
+}
