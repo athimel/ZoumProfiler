f439cc11079adf9311c14d54961fba32f4a13460
Arnaud Thimel <thimel@codelutin.com>
2015-01-23 11:19:57 +0100

    Meilleur interaction utilisateur pour les share/unshare

--- src/baseProfileController.js
+++ src/baseProfileController.js
@@ -601,17 +601,21 @@ angular.module('ZoumProfiler', ['ui.bootstrap', 'ngSanitize'])
 
         $scope.submitShare = function() {
             sharing.share($scope.profile, $scope.shareContext.user, $scope.shareContext.group).then(function(result) {
-                var share = {};
-                if ($scope.shareContext.user && $scope.shareContext.user.length > 0) {
-                    share.user = {};
-                    $scope.usersIndex[$scope.shareContext.user] = { login:$scope.shareContext.user }; // FIXME Vieux hack !
-                    share.user['$id'] = $scope.shareContext.user;
+                if (result.data.result == "SHARED") {
+                    var share = {};
+                    if ($scope.shareContext.user && $scope.shareContext.user.length > 0) {
+                        share.user = {};
+                        $scope.usersIndex[$scope.shareContext.user] = {login: $scope.shareContext.user}; // FIXME Vieux hack !
+                        share.user['$id'] = $scope.shareContext.user;
+                    }
+                    if ($scope.shareContext.group && $scope.shareContext.group.length > 0) {
+                        share.group = $scope.shareContext.group;
+                    }
+                    $scope.profile._internal.shares.push(share);
+                    $scope.cancelShare();
+                } else {
+                    $scope._addErrorMessage("Échec : " + result.data.result);
                 }
-                if ($scope.shareContext.group && $scope.shareContext.group.length > 0) {
-                    share.group = $scope.shareContext.group;
-                }
-                $scope.profile._internal.shares.push(share);
-                $scope.cancelShare();
             });
         };
 
@@ -621,7 +625,11 @@ angular.module('ZoumProfiler', ['ui.bootstrap', 'ngSanitize'])
 
         $scope.unshare = function(share) {
             sharing.unshare($scope.profile, share.user, share.group).then(function(result) {
-                $scope.profile._internal.shares.splice($scope.profile._internal.shares.indexOf(share), 1);
+                if (result.data.result == "UNSHARED") {
+                    $scope.profile._internal.shares.splice($scope.profile._internal.shares.indexOf(share), 1);
+                } else {
+                    $scope._addErrorMessage("Échec : " + result.data.result);
+                }
             });
         };
 
